dedde5959188cbd56836bae60308f25120f21b01
Dave Webb <dave.webb@cranbim.com>
2021-02-22 10:19:42 +0000

    update featureFlower

--- featureFlower/sketch.js
+++ featureFlower/sketch.js
@@ -1,21 +1,27 @@
-var feature;
+var features=[];
 
 function setup() {
   createCanvas(600, 400);
-  feature=new Feature(300,350,-PI*0.45,200,75);
+  for(var i=0; i<5; i++){
+    features.push(new Feature(200+i*50,350,-PI*(0.5+random(-0.05,0.05)),random(150,250),75));
+  }
 }
 
 function draw() {
   background(120);
-  feature.run();
+  features.forEach(function(feature){
+    feature.run();
+  });
 }
 
 function mousePressed() {
-feature.click();
+  features.forEach(function(feature){
+    feature.click();
+  });
 }
 
 function Feature(x,y,a,l,s){
-  flower=new FeatureFlower(x,y,a,l,s);
+  var flower=new FeatureFlower(x,y,a,l,s);
 
   this.click=function(){
     flower.toggle();
@@ -30,8 +36,8 @@ function Feature(x,y,a,l,s){
 function FeatureFlower(rx,ry,ra,ml,ms){
   var grow=0;
   var expand=0;
-  var growRate=0.025;
-  var expandRate=0.05;
+  var growRate=0.025*random(0.8,1.2);
+  var expandRate=0.05*random(0.8,1.2);
   var growOn=false;
   var expandOn=false;
   var relX=relY=0;
@@ -45,26 +51,27 @@ function FeatureFlower(rx,ry,ra,ml,ms){
     if(growOn){
       if(grow<1){
         grow+=growRate;
-      } else if(grow>0){
-        if(expand>0){
-          expand-=expandRate;
-        } else {
-          grow-=growRate;
-        }
+      }
+    } else if(grow>0){
+      if(expand>0){
+        expand-=expandRate;
+      } else {
+        grow-=growRate;
       }
     }
     if(expandOn && grow>=0.99){
       if(expand<1){
         expand+=expandRate;
-      } else if(expand>0){
-        expand-=expandRate;
       }
+    } else if(expand>0){
+      expand-=expandRate;
     }
     relX=cos(ra)*grow*ml;
     relY=sin(ra)*grow*ml;
-    flowerS=ms*0.1+expand*0.9;
+    flowerS=ms*(0.1+expand*0.9);
     hover=dist(mouseX, mouseY,rx+relX, ry+relY)<flowerS/2;
-    expandOn=hover;
+    expandOn=hover && growOn;
+    // console.log(expandOn);
   };
 
   this.show=function(){
