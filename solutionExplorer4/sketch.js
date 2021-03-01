2f15a9dd423cc5127db18616ae053fd7b7ad95f9
Dave Webb <dave.webb@cranbim.com>
2021-03-01 17:48:31 +0000

    initialise solutionExplorer4

--- solutionExplorer4/sketch.js
+++ solutionExplorer4/sketch.js
@@ -1,3 +1,5 @@
+
+
 const SELECTOR=0;
 const SOLUTION=1;
 const ASPECT=2;
@@ -187,7 +189,6 @@ function Explorer(x,y,s){
     for (var i=0; i<n; i++){
       aspects.push(new Aspect(i,i*aSpan,aSpan,s));
     }
-    console.log(aspects);
 
     this.click=function(){
       var selected=null;
@@ -220,30 +221,30 @@ function Explorer(x,y,s){
       strokeWeight(s*0.1);
       line(this.origin.x, this.origin.y, this.currX, this.currY);
       translate(this.currX, this.currY);
-      fill(this.hover?200:80);
-      noStroke();
-      ellipse(0,0,s);
-      fill(this.hover?0:255);
-      textAlign(CENTER, CENTER);
-      textSize(s/2);
-      text(id,0,0);
+      // fill(this.hover?200:80);
+      // noStroke();
+      // ellipse(0,0,s);
+      // fill(this.hover?0:255);
+      // textAlign(CENTER, CENTER);
+      // textSize(s/2);
+      // text(id,0,0);
       pop();
-      mouseA=atan2(mouseY-transform(self.currY,height/2,scl), mouseX-transform(self.currX, width/2,scl));
-      if(mouseA<0){
-        mouseA=TWO_PI+mouseA;
-      }
-      var d=dist(mouseX, mouseY, transform(self.currX, width/2,scl), transform(self.currY,height/2,scl));
-      stroke(255,0,0);
-      strokeWeight(1);
-      push();
-      translate(self.currX, self.currY);
-      rotate(mouseA);
-      line(0,0,d/currentScale,0);
-      pop();
-      aspects.forEach(function(aspect){
-        aspect.show(currentA, self.currX, self.currY, scl, mouseA,d);
-
-      });
+      // mouseA=atan2(mouseY-transform(self.currY,height/2,scl), mouseX-transform(self.currX, width/2,scl));
+      // if(mouseA<0){
+      //   mouseA=TWO_PI+mouseA;
+      // }
+      // var d=dist(mouseX, mouseY, transform(self.currX, width/2,scl), transform(self.currY,height/2,scl));
+      // stroke(255,0,0);
+      // strokeWeight(1);
+      // push();
+      // translate(self.currX, self.currY);
+      // rotate(mouseA);
+      // line(0,0,d/currentScale,0);
+      // pop();
+      // aspects.forEach(function(aspect){
+      //   aspect.show(currentA, self.currX, self.currY, scl, mouseA,d);
+      //
+      // });
     };
   }
 
@@ -319,123 +320,3 @@ function Explorer(x,y,s){
   }
 
 }
-
-// function Solution(space,sNom,n){
-//   var aspects=[];
-//   for(var i=0; i<n; i++){
-//     aspects.push(new Aspect());
-//   }
-//
-//   var aNow=0.1;
-//   var xNow=0;
-//   var yNow=0;
-//   var sclNow=0;
-//   var ease=20;
-//   var s=sNom;
-//   var aTarget=space.aFull;
-//
-//   this.show=function(exposure){
-//     if(exposure==FULL){
-//       xNow+=(space.xFull-xNow)/ease;
-//       yNow+=(space.yFull-yNow)/ease;
-//       sclNow+=(space.sclFull-sclNow)/ease;
-//       aNow+=(aTarget-aNow)/ease;
-//     }
-//     if(exposure==PARTIAL){
-//       xNow+=(space.xPartial-xNow)/ease;
-//       yNow+=(space.yPartial-yNow)/ease;
-//       sclNow+=(space.sclPartial-sclNow)/ease;
-//       aNow+=(aTarget-aNow)/ease;
-//     }
-//     push();
-//     translate(xNow, yNow);
-//     rotate(aNow*TWO_PI);
-//     scale(sclNow);
-//     noStroke();
-//     fill(0,0,100);
-//     ellipse(0,0,s);
-//     for(var i=0; i<n; i++){
-//       rotate(TWO_PI/n);
-//       stroke(0,0,100);
-//       noFill();
-//       strokeWeight(s*0.09);
-//       strokeCap(SQUARE);
-//       arc(0,0,s*1.2,s*1.2,0,0.9*TWO_PI/n);
-//     }
-//     pop();
-//   };
-//
-//   function Aspect(id,a){
-//     this.show=function(){
-//
-//     };
-//
-//   }
-// }
-//
-// function Selector(space,sNom,n){
-//   var options=[];
-//   for(var i=0; i<n; i++){
-//     options.push(new Option());
-//   }
-//   var aNow=0.1;
-//   var xNow=0;
-//   var yNow=0;
-//   var sclNow=0;
-//   var ease=20;
-//   var s=sNom;
-//   var aTarget=space.aFull;
-//
-//   this.show=function(exposure){
-//     if(exposure==FULL){
-//       xNow+=(space.xFull-xNow)/ease;
-//       yNow+=(space.yFull-yNow)/ease;
-//       sclNow+=(space.sclFull-sclNow)/ease;
-//       aNow+=(aTarget-aNow)/ease;
-//     }
-//     if(exposure==PARTIAL){
-//       xNow+=(space.xPartial-xNow)/ease;
-//       yNow+=(space.yPartial-yNow)/ease;
-//       sclNow+=(space.sclPartial-sclNow)/ease;
-//       aNow+=(aTarget-aNow)/ease;
-//     }
-//     push();
-//     translate(xNow, yNow);
-//     rotate(aNow*TWO_PI);
-//     scale(sclNow);
-//     noStroke();
-//     fill(0,0,100);
-//     ellipse(0,0,s);
-//     for(var i=0; i<n; i++){
-//       rotate(TWO_PI/n);
-//       stroke(0,0,100);
-//       strokeWeight(s*0.05);
-//       line(0,0,s,0);
-//       noStroke();
-//       fill(0,0,100);
-//       ellipse(s,0,s*0.25);
-//     }
-//     pop();
-//   };
-//
-//   function Option(id,a){
-//     this.show=function(){
-//
-//     };
-//
-//   }
-// }
-//
-// function Product(){
-//   var productImage=null;
-//   var productName="nothing";
-//   var productData={};
-//
-//   this.assignProductData=function(name, img, data){
-//     productName=name;
-//     productData=data;
-//     productImage=img;
-//   };
-//
-//
-// }
