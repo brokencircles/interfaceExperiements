b298f8868765b5a0499ecd998a466aca1f9ac2b4
Dave Webb <dave.webb@cranbim.com>
2021-03-08 10:54:58 +0000

    Working localStorage test

--- localStorageTest/persist.js
+++ localStorageTest/persist.js
@@ -1,41 +1,75 @@
 function PersistStore(n){
-  this.vals=[];
-  
+  var cache={
+    productClass: "",
+    productInClass: "",
+    story: "",
+    chapterInStory: ""
+  };
 
-  
+
   this.update=function(){
     this.updateStorage();
     console.log(this.vals);
   }
-  
+
   this.reset=function(){
-    this.vals=fillArrayWithFalse(n);
-    this.updateStorage();
+    if(localStorage.bmcStatus && localStorage.bmcStatus=="initialised"){
+      localStorage.removeItem("bmcStatus");
+      var storageKeys=Object.keys(cache);
+      storageKeys.forEach(function(key){
+        localStorage.removeItem(key);
+        cache[key]="";
+      });
+    }
+    // this.vals=fillArrayWithFalse(n);
+    // this.updateStorage();
   };
-  
-  this.updateStorage=function(){
-    this.vals.forEach(function(val,i){
-      localStorage.setItem(("fragment"+nf(i,2,0)),val?"true":"false");
+
+  this.updateStorage=function(newCache){
+    cache=newCache;
+    var storageKeys=Object.keys(cache);
+    storageKeys.forEach(function(key){
+      localStorage[key]=cache[key];
     });
   };
-  
+
+
   this.getStorage=function(){
-    for(var i=0; i<n; i++){
-      var res=localStorage.getItem("fragment"+nf(i,2,0));
-      this.vals[i]=res=="true";
-      // console.log(res,vals[i]);
+    return cache;
+    // for(var i=0; i<n; i++){
+    //   var res=localStorage.getItem("fragment"+nf(i,2,0));
+    //   this.vals[i]=res=="true";
+    //   // console.log(res,vals[i]);
+    // }
+  };
+
+  this.retrieveStorage=function(){
+    if(localStorage.bmcStatus && localStorage.bmcStatus=="initialised"){
+      var storageKeys=Object.keys(cache);
+      storageKeys.forEach(function(key){
+        cache[key]=localStorage[key];
+      });
     }
   };
-  
+
+  this.getStorageState=function(){
+    return localStorage.bmcStatus && localStorage.bmcStatus=="initialised";
+  };
+
   this.init=function(){
-    if(localStorage.fragmentArray && localStorage.fragmentArray=="thing"){
-      this.getStorage();
+    if(localStorage.bmcStatus && localStorage.bmcStatus=="initialised"){
+      this.retrieveStorage();
     } else {
-      localStorage.fragmentArray="thing";
-      this.vals=fillArrayWithFalse(n);
-      this.updateStorage();
+      localStorage.bmcStatus="initialised";
+      var storageKeys=Object.keys(cache);
+      storageKeys.forEach(function(key){
+        localStorage[key]="0";
+        cache[key]=0;
+      });
+      // this.vals=fillArrayWithFalse(n);
+      // this.updateStorage();
     }
-    console.log(this.vals);
+    console.log(cache);
   }
 
   this.init();
---
+++ localStorageTest/test.js
@@ -0,0 +1,42 @@
+var persist=new PersistStore(5);
+
+
+
+// checkAndUpdateStatus();
+
+function initialiseStore(){
+  persist.init();
+  checkAndUpdateStatus();
+}
+
+function checkAndUpdateStatus(){
+  console.log("check");
+  var status=persist.getStorageState();
+  document.getElementById("storage-status").innerHTML = (status?"initialised":"empty");
+  var vals=persist.getStorage();
+  document.getElementById("product-class").innerHTML = vals.productClass;
+  document.getElementById("product-in-class").innerHTML = vals.productInClass;
+  document.getElementById("story").innerHTML = vals.story;
+  document.getElementById("chapter-in-story").innerHTML = vals.chapterInStory;
+}
+
+function clearStorage(){
+  console.log("clear");
+  persist.reset();
+  checkAndUpdateStatus();
+}
+
+function changeValues(){
+  console.log("randomise");
+  if(!persist.getStorageState()){
+    persist.init();
+  } else {
+    var vals=persist.getStorage();
+    var values=Object.keys(vals);
+    values.forEach(function(val){
+      vals[val]=""+((parseInt(vals[val],10)+1)%5);
+    });
+    persist.updateStorage(vals);
+  }
+  checkAndUpdateStatus();
+}
